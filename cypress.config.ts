d526402e2b158be841616fa5153fee1689a00584
Mateo Agudelo Garcia <mateo.agudelo80@eia.edu.co>
2025-04-06 14:15:05 -0500

    feat: add retries and improve html name

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,17 +4,14 @@ export default defineConfig({
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/results',
+    reportFilename: 'index',
     overwrite: true,
     html: true,
     json: false,
   },
+  retries: 3,
   e2e: {
     baseUrl: 'https://petstore.octoperf.com',
     specPattern: 'cypress/e2e/**/*.cy.ts',
-    /*
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    */
   },
 });
